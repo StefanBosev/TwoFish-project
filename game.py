316510fd7df8874102ac8223afb049a457d3b760
StefanBosev <46899724+StefanBosev@users.noreply.github.com>
2019-08-13 16:55:09 +0300

    Almost working game

---
+++ game.py
@@ -0,0 +1,97 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.properties import StringProperty, NumericProperty
+from kivy.core.image import Image as CoreImage
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from random import randint
+
+Builder.load_file('my.kv')
+
+class MyGame(Widget):
+    counter = 0
+    right_pos = 0
+    q_index = 0
+    questions = [['ball',
+                    'image/ball.jpeg',
+                    'image/partyhat.jpg',
+                    'image/pizza.jpg',
+                    'image/star.jpg'],
+                ['party hat',
+                    'image/partyhat.jpg',
+                    'image/ball.jpeg',
+                    'image/pizza.jpg',
+                    'image/star.jpg'],
+                ['pizza',
+                    'image/pizza.jpg',
+                    'image/star.jpg',
+                    'image/ball.jpeg',
+                    'image/partyhat.jpg'],
+                ['star',
+                    'image/star.jpg',
+                    'image/partyhat.jpg',
+                    'image/ball.jpeg',
+                    'image/pizza.jpg']
+                ]
+
+    questions_len = len(questions)
+
+    def r_count(self):
+        self.counter += 1
+        print(self.counter)
+
+    def l_count(self):
+        self.counter -= 1
+        print(self.counter)
+
+    def set_question(self):
+        self.q_index = randint(0, self.questions_len - 1)
+        self.right_pos = randint(1, 4)
+        print(self.right_pos)
+
+    def choice(self, name):
+        if name == self.right_pos:
+            self.r_count()
+        else:
+            if self.counter < 1:
+                print(self.counter)
+            else:
+                self.l_count()
+
+
+class MenuScreen(Screen):
+    pass
+
+class StudentScreen(Screen):
+    pass
+
+class TeacherScreen(Screen):
+    pass
+
+class GameScreen(Screen):
+    game = MyGame()
+    def build(self):
+        return game.run()
+
+class WinScreen(Screen):
+    pass
+
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name = "menu"))
+sm.add_widget(StudentScreen(name = "stud_scr"))
+sm.add_widget(TeacherScreen(name = "teach_scr"))
+sm.add_widget(GameScreen(name = "game"))
+sm.add_widget(WinScreen(name = "win_screen"))
+
+sm.current = 'menu'
+
+
+class MyApp(App):
+
+    def build(self):
+        return sm
+
+if __name__ == '__main__':
+    MyApp().run()
